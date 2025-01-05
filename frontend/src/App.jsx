85e72671230aaf6b3212561ee6e5c9ebafd20ffb
Duub3m <deneh03@gmail.com>
2025-01-04 23:55:42 -0500

    Added front end for updating contact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,27 +4,51 @@ import "./App.css";
 import ContactForm from "./ContactForm";
 
 function App() {
-  const [contacts, setContacts] = useState([]); // Fixed typo in setContacts
+  const [contacts, setContacts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [currentContact, setCurrentContact] = useState({})
 
   useEffect(() => {
-    fetchContacts();
+    fetchContacts()
   }, []);
 
   const fetchContacts = async () => {
-    try {
-      const response = await fetch("http://127.0.0.1:5000/contacts");
-      const data = await response.json();
-      setContacts(data.contacts);
-      console.log(data.contacts);
-    } catch (error) {
-      console.error("Error fetching contacts:", error);
-    }
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
   };
+
+  const closeModal = () => {
+    setIsModalOpen(false)
+    setCurrentContact({})
+  }
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true)
+  }
+
+  const openEditModal = (contact) => {
+    if (isModalOpen) return
+    setCurrentContact(contact)
+    setIsModalOpen(true)
+  }
+
+  const onUpdate = () => {
+    closeModal()
+    fetchContacts()
+  }
 
   return (
     <>
-      <ContactList contacts={contacts} />
-      <ContactForm refreshContacts={fetchContacts} /> {/* Pass fetchContacts to ContactForm */}
+      <ContactList contacts={contacts} updateContact={openEditModal} updateCallback={onUpdate} />
+      <button onClick={openCreateModal}>Create New Contact</button>
+      {isModalOpen && <div className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={closeModal}>&times;</span>
+          <ContactForm existingContact={currentContact} updateCallback={onUpdate} />
+        </div>
+      </div>
+      }
     </>
   );
 }
